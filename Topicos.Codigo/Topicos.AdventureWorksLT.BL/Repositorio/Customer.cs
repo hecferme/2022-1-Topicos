0bb600e2102810ae557b5a6ed74781c02f398415
HÉCTOR FERNÁNDEZ MÉNDEZ <FernandezMH@bccr.fi.cr>
2022-02-12 16:35:10 -0600

    consultas linq avanzadas

--- Topicos.Codigo/Topicos.AdventureWorksLT.BL/Repositorio/Customer.cs
+++ Topicos.Codigo/Topicos.AdventureWorksLT.BL/Repositorio/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,15 @@ namespace Topicos.AdventureWorksLT.BL.Repositorio
     internal class Customer
     {
         private readonly db_adventureworksltContext _contexto = new();
+        private readonly int _pagesize = 10;
+        public Customer()
+        {
+        }
+
+        public Customer(int pagesize)
+        {
+            _pagesize = pagesize;
+        }
 
         public Model.Models.Customer? BuscarPorId (int id)
         {
@@ -17,10 +27,29 @@ namespace Topicos.AdventureWorksLT.BL.Repositorio
             return resultado;
         }
 
-        public IList<Model.Models.Customer> BuscarPorNombreOApellido (string hilera)
+        public IList<Model.Models.Customer> BuscarPorNombreOApellido (string hilera, int pageNumber = 0)
         {
-            var resultado = _contexto.Customers.Where(c => c.FirstName.Contains(hilera) || c.LastName.Contains (hilera)).OrderBy(c => c.FirstName).ToList();
+            var resultado = _contexto.Customers.Where(
+                c => c.FirstName.Contains(hilera) || c.LastName.Contains (hilera))
+                .Skip(_pagesize * pageNumber).Take(_pagesize).
+                OrderBy(c => c.FirstName).ToList();
             return resultado;
         }
+
+        /// <summary>
+        /// Lista todos los customers que tienen una dirección en un país determinado.
+        /// </summary>
+        /// <param name="hilera"></param>
+        /// <param name="pageNumber"></param>
+        /// <returns></returns>
+        public IList<Model.Models.Customer> BuscarPorCountry(string hilera, int pageNumber = 0)
+        {
+            var resultado = _contexto.Customers.Include(c => c.CustomerAddresses).ThenInclude(ca => ca.Address).Where(
+                c => c.CustomerAddresses.Any(a => a.Address.CountryRegion.Contains (hilera)))
+                .Skip(_pagesize * pageNumber).Take(_pagesize).
+                OrderBy(c => c.FirstName).ToList();
+            return resultado;
+        }
+
     }
 }
